9457069a70ce94d483911fab93c968979934ced1
ilyava1 <ilyava1@yandex.ru>
2022-11-22 22:04:52 +0300

    Request processing and templates part

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,12 +19,67 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def ingrs_multiplication(choisen_dish, servings):
+    """
+    Функция перемножения ингредиентов на количество персон
+
+    :param choisen_dish: словарь с блюдами, ингридентами и их кол-ом
+    :param servings: количество персон
+    :return: словарь с блюдами, ингридентами и их кол-ом в расчета на
+    количество персон servings
+    """
+    for key, item in choisen_dish.items():
+        new_ingrs = {}
+        for ingr, qua in item.items():
+            new_ingrs[ingr] = qua * servings
+        choisen_dish[key] = new_ingrs
+    return choisen_dish
+
+
+def home_view(request):
+    """
+    Функция формирования контекста для стартовой страницы сервиса
+    :param request:
+    :return: результат работы функции render
+    """
+    template_name = 'calculator/index.html'
+    my_context = {}
+    my_context['data'] = DATA
+    context = my_context
+    return render(request, template_name, context)
+
+
+def prepare_dish(request, user_choise):
+    """
+    Функция формирования контекста для страницы блюда
+
+    Функция формирует словарь choisen_dish из одной пары ключ-значение:
+    блюдо-список_ингредиентов. Затем проверяет есть ли в запросе параметр
+    servings - количество персон. Если нет, количество персон принимается
+    за единицу, если есть - то данное значение извлекается. Далее choisen_dish
+    и servings передается в функцию ingrs_multiplication для расчета кол-ва
+    ингредиентов на заданное кол-во персон. Возращается словарь с перечнем и
+    кол-ом ингредиентов, который вместе с шаблоном страницы передается в
+    функцию render
+    :param request:
+    :return: результат работы функции render
+    """
+    template_name = 'calculator/recipes.html'
+    my_context = {}
+    choisen_dish = {}
+    if user_choise in DATA.keys():
+        choisen_dish[user_choise] = DATA[user_choise]
+
+        if 'servings' in request.GET:
+            servings = int(request.GET['servings'])
+        else:
+            servings = 1
+        choisen_dish = ingrs_multiplication(choisen_dish, servings)
+    else:
+        choisen_dish[user_choise] = {}
+
+    my_context['data'] = choisen_dish
+    context = my_context
+
+    return render(request, template_name, context)
